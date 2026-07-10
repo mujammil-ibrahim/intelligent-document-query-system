b60938eee16867ddf0d193cbeab59cb64b657a6a
Mujammil Ibrahim <mujammilibrahim007@gmail.com>
2026-07-10 15:10:38 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://intelligent-document-query-system.onrender.com"
 
 st.set_page_config(
     page_title="Intelligent Document Query System",
